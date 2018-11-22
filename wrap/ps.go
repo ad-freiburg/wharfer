b384d294ae3bff6c02c0a2c9fb867011d180d60b
Axel Lehmann <lehmann.4178656ch@gmail.com>
2018-11-22 14:23:06 +0100

    Add -a to 'wharfer ps'

--- wrap/ps.go
+++ wrap/ps.go
@@ -6,13 +6,17 @@ import (
 
 type Ps struct {
 	Cmd    *flag.FlagSet
-	Quiet  bool
+	All    bool
 	Filter string
+	Quiet  bool
 }
 
 func (ps *Ps) InitFlags() {
+	const allUsage = "Show all containers (default shows just running)"
 	const filterUsage = "Filter output based on conditions provided"
 	ps.Cmd = flag.NewFlagSet("ps", flag.ExitOnError)
+	ps.Cmd.BoolVar(&ps.All, "all", false, allUsage)
+	ps.Cmd.BoolVar(&ps.All, "a", false, allUsage+" (shorthand)")
 	ps.Cmd.StringVar(&ps.Filter, "filter", "", filterUsage)
 	ps.Cmd.StringVar(&ps.Filter, "f", "", filterUsage+" (shorthand)")
 	ps.Cmd.BoolVar(&ps.Quiet, "q", false, "Only display numeric IDs")
@@ -21,11 +25,14 @@ func (ps *Ps) InitFlags() {
 func (ps *Ps) ParseToArgs(rawArgs []string) []string {
 	ps.Cmd.Parse(rawArgs)
 	args := []string{"ps"}
-	if ps.Quiet {
-		args = append(args, "-q")
+	if ps.All {
+		args = append(args, "-a")
 	}
 	if ps.Filter != "" {
 		args = append(args, "--filter", ps.Filter)
+	}
+	if ps.Quiet {
+		args = append(args, "-q")
 	}
 	return args
 }
