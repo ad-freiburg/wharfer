721810bb9233e5d0cacad6b38d7f425e416bba84
Niklas Schnelle <schnelle@informatik.uni-freiburg.de>
2018-12-19 16:38:03 +0100

    Add -f flag to wharfer rm fixes #9

--- wrap/rm.go
+++ wrap/rm.go
@@ -5,16 +5,23 @@ import (
 )
 
 type Rm struct {
-	Cmd *flag.FlagSet
+	Cmd   *flag.FlagSet
+	Force bool
 }
 
 func (rm *Rm) InitFlags() {
 	rm.Cmd = flag.NewFlagSet("rm", flag.ExitOnError)
+	rm.Cmd.BoolVar(&rm.Force, "f", false, "Force removal also kills running container")
 }
 
 func (rm *Rm) ParseToArgs(rawArgs []string) []string {
 	rm.Cmd.Parse(rawArgs)
 	args := []string{"rm"}
+
+	if rm.Force {
+		args = append(args, "-f")
+	}
+
 	if rm.Cmd.NArg() > 0 {
 		// add -- to make sure additional arguments are not interpreted as
 		// potentially harmful flags. Here this is the container to rm
