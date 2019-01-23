a7be7150eb8274f3c6a8c665156cc44661dcb069
Axel Lehmann <lehmann.4178656ch@gmail.com>
2019-01-23 14:19:55 +0100

    PrependUsername in rm command

--- wrap/rm.go
+++ wrap/rm.go
@@ -26,7 +26,12 @@ func (rm *Rm) ParseToArgs(rawArgs []string) []string {
 		// add -- to make sure additional arguments are not interpreted as
 		// potentially harmful flags. Here this is the container to rm
 		args = append(args, "--")
-		args = append(args, rm.Cmd.Args()...)
+		for _, arg := range kill.Cmd.Args() {
+			if !IsHexOnly(arg) {
+				arg = PrependUsername(arg)
+			}
+			args = append(args, arg)
+		}
 	}
 	return args
 }
